f3ac96ece94ccaf67cc3adab1388f12f5d1029a9
vivek <vivektiwarivip2@gmail.com>
2023-05-31 10:23:56 +0530

    React JS Project
    FSJS 2.0

---
+++ Movie app react vite Project No 5/src/App.jsx
@@ -0,0 +1,30 @@
+
+import './App.css'
+import Header from './Components/Header'
+import Movie from './Components/Movie'
+import data from './movie.json'
+
+
+function App() {
+  return (
+    <div className='App'>
+      <Header />
+      <div className='main'>
+        {
+        data.map((element)=>{
+          return(
+          <Movie 
+          title={element.Title} 
+          year={element.Year}
+          img={element.Poster} 
+          />
+          )
+        })
+        }
+      </div>
+     </div>
+    )
+}
+
+
+export default App
